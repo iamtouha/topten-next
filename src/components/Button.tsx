3fdb7757e319fc4532fca08cf28c5fc9317ce45b
Touha Zohair <touha98@gmail.com>
2022-12-14 14:35:46 +0600

    button props and and desin update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,30 @@
-import React from "react";
+import type { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
 type ButtonProps = {
   intent: "primary" | "secondary" | "success" | "danger";
-  text: string;
-  onClick?: (() => void) | (() => Promise<undefined>);
-};
 
-const Button = ({ intent, text, onClick }: ButtonProps) => {
+  children: ReactNode;
+} & DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+const Button = ({ intent, children, className, ...btnProps }: ButtonProps) => {
   return (
     <button
       aria-label={`${intent} button`}
-      className={`w-fit rounded-md px-3.5 py-2 text-sm font-semibold text-white transition-opacity hover:opacity-80 focus:outline-none active:opacity-100 ${
+      className={`w-fit rounded-none px-3.5 py-2 text-sm font-semibold text-white transition-opacity hover:opacity-80 focus:outline-none active:opacity-100 ${
         intent === "primary"
-          ? "bg-primary"
+          ? "bg-primary-700"
           : intent === "secondary"
           ? "bg-secondary"
           : intent === "success"
           ? "bg-success"
           : "bg-danger"
-      }`}
-      onClick={() => onClick && onClick()}
+      } ${className}`}
+      {...btnProps}
     >
-      {text}
+      {children}
     </button>
   );
 };
