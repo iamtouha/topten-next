e8d70f6b3ec311079c0b0eec2da4745f18fe3e47
Touha Zohair <touha98@gmail.com>
2022-12-15 14:06:35 +0600

    trpc user route

---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,30 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+
+export const userRouter = router({
+  createProfile: protectedProcedure
+    .input(
+      z.object({
+        fullName: z.string().min(5),
+        phone: z.string().min(11).max(15),
+        designation: z.string().min(4).max(32),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (ctx.session.user.profileId) {
+        throw new TRPCError({
+          message: "User profile already exists",
+          code: "FORBIDDEN",
+        });
+      }
+      return await ctx.prisma.profile.create({
+        data: {
+          user: { connect: { id: ctx.session.user.id } },
+          fullName: input.fullName,
+          phone: input.phone,
+          designation: input.designation,
+        },
+      });
+    }),
+});
