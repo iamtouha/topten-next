d4c3cccfc4baf638762d7cb3fccc03d64b97ed4d
Touha Zohair <touha98@gmail.com>
2022-12-26 00:02:26 +0600

    stores api

--- src/server/trpc/router/admin/stores.ts
+++ src/server/trpc/router/admin/stores.ts
@@ -1,58 +1,53 @@
-import { type Prisma } from "@prisma/client";
+import { STORE_TYPE, type Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { router, adminProcedure } from "../../trpc";
 
 export const storesAdminRouter = router({
+  list: adminProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.store.findMany();
+  }),
   get: adminProcedure
     .input(
       z.object({
         page: z.number().int().default(0),
         perPage: z.number().int().default(10),
         name: z.string().optional(),
-        size: z.string().optional(),
-        sortBy: z
-          .enum(["name", "size", "published", "price", "createdAt"])
-          .optional(),
+        sortBy: z.enum(["name", "published", "type", "createdAt"]).optional(),
         sortDesc: z.boolean().default(false),
       })
     )
     .query(async ({ ctx, input }) => {
-      const needFilter = input.size || input.name;
+      const needFilter = input.name;
 
-      const params: Prisma.ProductFindManyArgs = {
+      const params: Prisma.StoreFindManyArgs = {
         orderBy: input.sortBy
           ? { [input.sortBy]: input.sortDesc ? "desc" : "asc" }
           : undefined,
         where: needFilter
-          ? {
-              AND: {
-                name: input.name ? { contains: input.name } : undefined,
-                size: input.size ? { contains: input.size } : undefined,
-              },
-            }
+          ? { name: input.name ? { contains: input.name } : undefined }
           : undefined,
       };
 
-      const [count, products] = await ctx.prisma.$transaction([
-        ctx.prisma.product.count({ where: params.where }),
-        ctx.prisma.product.findMany({
+      const [count, stores] = await ctx.prisma.$transaction([
+        ctx.prisma.store.count({ where: params.where }),
+        ctx.prisma.store.findMany({
           ...params,
           skip: input.page * input.perPage,
           take: input.perPage,
         }),
       ]);
-      return { count, products };
+      return { count, stores };
     }),
   getOne: adminProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    const product = await ctx.prisma.product.findUnique({
+    const store = await ctx.prisma.store.findUnique({
       where: { id: input },
-      include: { stocks: true },
+      include: { roles: true },
     });
-    if (!product) {
+    if (!store) {
       throw new TRPCError({ code: "NOT_FOUND", message: "User not found!" });
     }
-    return product;
+    return store;
   }),
 
   create: adminProcedure
@@ -60,17 +55,18 @@ export const storesAdminRouter = router({
       z.object({
         name: z.string(),
         address: z.string(),
-        price: z.number().min(0),
+        type: z.nativeEnum(STORE_TYPE),
         published: z.boolean().default(true),
       })
     )
     .mutation(({ ctx, input }) => {
-      const { name, address, price, published } = input;
+      const { name, address, type, published } = input;
       return ctx.prisma.store.create({
         data: {
           name,
           address,
           published,
+          type,
           createdBy: ctx.session.user.email,
         },
       });
@@ -81,19 +77,18 @@ export const storesAdminRouter = router({
       z.object({
         id: z.string(),
         name: z.string(),
-        size: z.string(),
-        price: z.number().min(0),
+        address: z.string(),
+        type: z.nativeEnum(STORE_TYPE),
         published: z.boolean().default(true),
       })
     )
     .mutation(({ ctx, input }) => {
-      const { id, name, size, price, published } = input;
-      return ctx.prisma.product.update({
+      const { id, name, address, published } = input;
+      return ctx.prisma.store.update({
         where: { id },
         data: {
           name,
-          size,
-          price,
+          address,
           published,
           updatedBy: ctx.session.user.email,
         },
@@ -101,6 +96,6 @@ export const storesAdminRouter = router({
     }),
 
   delete: adminProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
-    return await ctx.prisma.product.delete({ where: { id: input } });
+    return await ctx.prisma.store.delete({ where: { id: input } });
   }),
 });
